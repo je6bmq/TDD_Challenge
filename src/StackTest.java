76a10e9c430f5a8b3c5af9ebcbd384fec9acdde2
je6bmq <>
2017-08-18 12:48:31 +0900

    add push test

--- src/StackTest.java
+++ src/StackTest.java
@@ -8,10 +8,15 @@ public class StackTest extends TestCase{
 	public void test() {
 		//fail("まだ実装されていません");
 	}
-
+	@Test
 	public void testCreate(){
 		Stack stack = new Stack();
 		assertTrue(stack.isEmpty());
 	}
-
+	@Test
+	public void testPush() {
+		Stack stack= new Stack();
+		stack.push(1);
+		assertFalse(stack.isEmpty());
+	}
 }
