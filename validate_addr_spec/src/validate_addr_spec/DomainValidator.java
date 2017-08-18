2c920e6d5c9ef28f7c0842b05359a3162f0b8d57
je6bmq <>
2017-08-18 17:15:14 +0900

    modify to singleton

--- validate_addr_spec/src/validate_addr_spec/DomainValidator.java
+++ validate_addr_spec/src/validate_addr_spec/DomainValidator.java
@@ -1,19 +1,19 @@
 package validate_addr_spec;
 
 public class DomainValidator {
-    boolean isValidD1(String domain) {
+    public static boolean isValidD1(String domain) {
         return true;
     }
-    boolean isValidD2(String domain) {
+    public static boolean isValidD2(String domain) {
         return true;
     }
-    boolean isValidD3(String domain) {
+    public static boolean isValidD3(String domain) {
         return true;
     }
-    boolean isValidD4(String domain) {
+    public static boolean isValidD4(String domain) {
         return true;
     }
-    boolean isValidD5(String domain) {
+    public static boolean isValidD5(String domain) {
         return true;
     }
 }
