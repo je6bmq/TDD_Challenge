aefc1ea34309594dc374b46cc66668fb88a293e0
daifunayama <s1210121a@gmail.com>
2017-08-18 12:32:52 +0900

    isEmptyのテスト

---
+++ src/StackTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class StackTest {
+
+	@Test
+	public void test() {
+		//fail("まだ実装されていません");
+		Stack stack = new Stack();;
+
+		assertTrue(stack.isEmpty());
+	}
+
+}
