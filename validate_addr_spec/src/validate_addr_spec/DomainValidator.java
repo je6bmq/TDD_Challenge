6b67eab6561beb2afd69c5f08d8b0c486c59f8af
je6bmq <>
2017-08-18 17:14:06 +0900

    add DomainValidator

---
+++ validate_addr_spec/src/validate_addr_spec/DomainValidator.java
@@ -0,0 +1,19 @@
+package validate_addr_spec;
+
+public class DomainValidator {
+    boolean isValidD1(String domain) {
+        return true;
+    }
+    boolean isValidD2(String domain) {
+        return true;
+    }
+    boolean isValidD3(String domain) {
+        return true;
+    }
+    boolean isValidD4(String domain) {
+        return true;
+    }
+    boolean isValidD5(String domain) {
+        return true;
+    }
+}
